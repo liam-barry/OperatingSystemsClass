97089a482f83d4597ce9a2cc6a88e8a61de1e76e
Liam Barry <lbarry1@students.stonehill.edu>
2024-09-20 16:30:03 -0400

    Initial commit of OpSystemsStart.cpp

---
+++ OpSystemsStart.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // Your code goes here
+    for(int i = 0 ; i < 100 ; i++){
+        if(i % 2 == 0){
+            std::cout << i << std::endl;
+        }
+    }
+    return 0;
+}
